ecc83e84896fddfdb96a1d1aa6b9bc4be871820c
paian <pai_an@outlook.com>
2022-02-28 19:13:27 +0800

    build: add support for TypeScript

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,19 @@
 const CracoLessPlugin = require('craco-less');
+const path = require('path/posix');
 
 module.exports = {
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      webpackConfig.entry = path.resolve(__dirname, './src/index.tsx');
+      webpackConfig.resolve.extensions = [
+        '.tsx',
+        '.ts',
+        ...webpackConfig.resolve.extensions,
+      ];
+
+      return webpackConfig;
+    }
+  },
   plugins: [
     {
       plugin: CracoLessPlugin,
