a307f396d6a81421d99ad04a7592d898de91dee1
paian <pai_an@outlook.com>
2022-03-01 01:59:10 +0800

    style: modify page padding

--- src/features/home/MainPage.tsx
+++ src/features/home/MainPage.tsx
@@ -5,7 +5,7 @@ function MainPage() {
   return (
     <div className="main-page">
       <h2 className="logo"><ThunderboltFilled /><span>Lottie Editor</span></h2>
-      <p className="brief">轻松制作精美动效</p>
+      <p className="brief">快速制作精美动效</p>
       <p className="description">通过精美的动效模版和可视化编辑界面，让你轻松获得满意的动效～</p>
       <a href="/editor" className="button">立即使用<ArrowRightOutlined /></a>
     </div>
