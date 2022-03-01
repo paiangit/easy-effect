968683d80db632a45a22f544fd07ad2da7ccdf48
paian <pai_an@outlook.com>
2022-03-02 01:49:46 +0800

    fix: import antd style

--- src/context/AppProviders.tsx
+++ src/context/AppProviders.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import '~styles/global.less';
 import '~styles/iconfont.less';
+import 'antd/dist/antd.less';
 
 function AppProviders({ children }: { children: ReactNode }) {
   return (
