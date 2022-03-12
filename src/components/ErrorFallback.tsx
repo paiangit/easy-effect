839ffb1ebeb176155fec6c1692240712d983d1c8
paian <pai_an@outlook.com>
2022-03-13 03:10:38 +0800

    feat: use error boundary

---
+++ src/components/ErrorFallback.tsx
@@ -0,0 +1,16 @@
+import { Button } from 'antd';
+import styles from './ErrorFallback.module.less';
+
+export function ErrorFallback({ error, resetErrorBoundary }) {
+  return (
+    <div className={ styles['error-fallback'] }>
+      <div className={ styles['inner'] }>
+        <p className={ styles['title'] }>出错了</p>
+        <pre className={ styles['message'] }>{error.message}</pre>
+        <Button className={ styles['button'] } type="primary" onClick={resetErrorBoundary}>
+          重试
+        </Button>
+      </div>
+    </div>
+  );
+}
--- src/context/AppProviders.tsx
+++ src/context/AppProviders.tsx
@@ -1,14 +1,18 @@
 import React, { ReactNode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { ErrorBoundary } from 'react-error-boundary';
+import { ErrorFallback } from '../components/ErrorFallback';
 import '~styles/global.less';
 import '~styles/iconfont.less';
 import 'antd/dist/antd.less';
 
 function AppProviders({ children }: { children: ReactNode }) {
   return (
-    <BrowserRouter>
-      {children}
-    </BrowserRouter>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <BrowserRouter>
+        {children}
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 };
 
