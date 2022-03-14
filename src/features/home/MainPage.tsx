b4e10bd1163981ca3aa4dbd9412e2524a9cd89ec
paian <pai_an@outlook.com>
2022-03-14 12:00:49 +0800

    refactor: optimize style

--- src/features/home/MainPage.tsx
+++ src/features/home/MainPage.tsx
@@ -19,12 +19,13 @@ function MainPage() {
   return (
     <div className={style['main-page']}>
       <Logo className={style.logo}/>
-      <p className={style.brief}>快速制作精美动效</p>
-      <p className={style.description}>
-        基于精美的动效模版，进行动效可视化编辑，导出的动效支持跨平台（H5、iOS、Android、ReactNative中均可用）。
-        <br/>完全开源免费，支持独立部署和二次开发。助您盘活动效资产，告别低效沟通和重复劳动～
-      </p>
-      <Button className={style.button} type="primary" onClick={handleClick}>立即使用<ArrowRightOutlined /></Button>
+      <div className={style.body}>
+        <p className={style.brief}>快速制作精美动效</p>
+        <p className={style.description}>
+          基于精美的动效模版，进行动效可视化编辑，导出的动效支持跨平台（H5/iOS/Android/ReactNative）。开源免费，支持独立部署和二次开发。助您盘活动效资产，告别低效沟通和重复劳动～
+        </p>
+        <Button className={style.button} type="primary" onClick={handleClick}>立即使用<ArrowRightOutlined /></Button>
+      </div>
       <Footer/>
     </div>
   );
